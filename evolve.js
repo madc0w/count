13bd0cd172dbb15fe871691abbde0c8539ff6689
Marc A. Donis <marc@balloonary.io>
2023-06-24 09:43:01 +0200

    .

--- evolve.js
+++ evolve.js
@@ -1,10 +1,21 @@
 const probReproduce = 0.2;
 const advantage = 0.001;
-// const maxPop = 2000;
+const maxPop = 2000;
 const t = 2000;
 
-const numNoAdvantage = (1 + probReproduce) ** t;
-const numAdvantage = (1 + probReproduce + advantage) ** t;
+let numNoAdvantage = 1;
+let numAdvantage = 1;
+
+for (let i = 0; i < t; i++) {
+	numNoAdvantage *= 1 + probReproduce;
+	numAdvantage *= 1 + probReproduce + advantage;
+
+	if (numNoAdvantage + numAdvantage > maxPop) {
+		const deathRatio = maxPop / (numNoAdvantage + numAdvantage);
+		numNoAdvantage *= deathRatio;
+		numAdvantage *= deathRatio;
+	}
+}
 const ratio = numAdvantage / (numAdvantage + numNoAdvantage);
 console.log(
 	`ratio of creatures with advantage (${numAdvantage.toFixed(
