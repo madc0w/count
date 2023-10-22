9aa4154dfd1c0767ed9cc81f24a5910b6501ee3c
Marc A. Donis <marc@balloonary.io>
2023-10-22 19:03:46 +0200

    .

--- ally-graph.js
+++ ally-graph.js
@@ -1,6 +1,52 @@
 const nodes = [];
 initGraph();
 
+for (let degree = 1; degree < 4; degree++) {
+	console.log(`degree ${degree} contradictions:`);
+	for (const node1 of nodes) {
+		for (const link1 of node1.links) {
+			const node2 = link1.node;
+			for (const link2 of node2.links) {
+				const node3 = link2.node;
+				const node1node3Link = findLink(node1, node3);
+				if (node1node3Link) {
+					if (
+						(link1.type == link2.type && node1node3Link.type == 'enemy') ||
+						(link1.type != link2.type && node1node3Link.type == 'ally')
+					) {
+						console.log(
+							`${node1.name} is ${link1.type} of ${node2.name}, who is ${link2.type} of ${node3.name}, but ${node1.name} is ${node1node3Link.degree}-degree ${node1node3Link.type} of ${node3.name}`
+						);
+					}
+				}
+			}
+		}
+	}
+
+	for (const node1 of nodes) {
+		for (const link1 of node1.links) {
+			const node2 = link1.node;
+			for (const link2 of node2.links) {
+				const node3 = link2.node;
+				const node1node3Link = findLink(node1, node3);
+				if (!node1node3Link) {
+					const type = link1.type == link2.type ? 'ally' : 'enemy';
+					const existingLink = link(node1.name, node3.name, type, degree, [
+						link1,
+						link2,
+					]);
+					if (existingLink && existingLink.type != type) {
+						console.log(
+							`*** failed to create ${type} link between ${node1.name} and ${node3.name} because of existing ${existingLink.type} link`
+						);
+					}
+				}
+			}
+		}
+	}
+	console.log('====================');
+}
+
 function initGraph() {
 	newNode('Israel');
 	newNode('Palestine');
@@ -12,6 +58,7 @@ function initGraph() {
 	newNode('US Left');
 	newNode('Hamas');
 	newNode('LGBT');
+	newNode('Marxists');
 
 	ally('Israel', 'Jews');
 	ally('Israel', 'US Right');
@@ -22,6 +69,7 @@ function initGraph() {
 	ally('Palestine', 'Muslims');
 	ally('Palestine', 'Hamas');
 	ally('Palestine', 'LGBT');
+	ally('Palestine', 'Marxists');
 	enemy('Palestine', 'Jews');
 	enemy('Palestine', 'US Right');
 
@@ -44,16 +92,19 @@ function initGraph() {
 	enemy('Christian Right', 'Palestine');
 	enemy('Christian Right', 'US Left');
 	enemy('Christian Right', 'LGBT');
+	enemy('Christian Right', 'Marxists');
 
 	ally('Nazis', 'Muslims');
 	enemy('Nazis', 'US Left');
 	enemy('Nazis', 'LGBT');
 	enemy('Nazis', 'Jews');
 	enemy('Nazis', 'Israel');
+	enemy('Nazis', 'Marxists');
 
 	ally('US Right', 'Christian Right');
 	ally('US Right', 'Israel');
 	enemy('US Right', 'Palestine');
+	enemy('US Right', 'Marxists');
 }
 
 function ally(name1, name2, degree = 0) {
@@ -64,25 +115,36 @@ function enemy(name1, name2, degree = 0) {
 	return link(name1, name2, 'enemy', degree);
 }
 
-function link(name1, name2, type, degree = 0) {
-	const existing1 = node(name1).allies.find((n) => n.node.name == name2);
+function link(name1, name2, type, degree = 0, cause) {
+	const node1 = node(name1);
+	const node2 = node(name2);
+	if (!node1 || !node2) {
+		return;
+	}
+	const existing1 = findLink(node1, node2);
 	if (existing1 && existing1.type != type) {
 		return existing1;
 	}
-	const existing2 = node(name2).allies.find((n) => n.node.name == name1);
+	const existing2 = findLink(node2, node1);
 	if (existing2 && existing2.type != type) {
 		return existing2;
 	}
-	node(name1).links.push({
+	node1.links.push({
 		degree,
-		node: node(name2),
+		node: node2,
 		type,
+		cause,
 	});
-	node(name2).links.push({
+	node2.links.push({
 		degree,
-		node: node(name1),
+		node: node1,
 		type,
+		cause,
 	});
+}
+
+function findLink(node1, node2) {
+	return node1.links.find((l) => l.node == node2);
 }
 
 function newNode(name) {
